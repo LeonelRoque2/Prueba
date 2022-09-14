025d3a958a6e9e0b1556ce60d19f3314867d2b1e
LeonelRoque2 <leoroque9@gmail.com>
2022-09-14 18:02:28 +0200

    Punto 5 del TP1

---
+++ src/packClases/PrimeraClase.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package packClases;
+
+/**
+ *
+ * @author Administrador
+ */
+public class PrimeraClase {
+    int unEntero;
+    String unaCadena;
+    public void mostrar(){
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
